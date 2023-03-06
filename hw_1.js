09e23e97f176b336fc7e16f6add53dd537ef41dc
George kuznecov <georgijkuznecov987@gmail.com>
2023-03-07 00:02:03 +0300

    Как сделать задание 9.4?

---
+++ hw_1.js
@@ -0,0 +1,44 @@
+/*let a = 10;
+alert(a)
+a += 10;
+alert(a)*/
+
+/*let age = "29.06.2007";
+alert("Дата выходы первого айфона " + age) ; */
+/*let Name = "Брендан Айк"
+alert("именz создателя языка JavaScript = " + Name)*/
+
+/*let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a / b);
+alert(a * b);*/
+/*let v = (2**5);
+alert(v)*/
+/*let a = 9;
+let b = 2;
+alert(a % b);*/
+
+/*let num = 1;
+num += 5;
+num -+ 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);*/
+/*let age = prompt("Сколько вам лет?");
+alert(age);*/
+
+/*let user = {
+    Name: 'George',
+    Age: 20,
+    isAdmin: true,
+    city_of_residence: String(Kazan),
+    info: prompt("Какую информацию хотите узнать о пользователе?")
+};
+alert(user.info);*/
+
+let data = prompt("Назовите имя и фамилию");
+alert("Привет ! " + data);
