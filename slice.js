b4c0c346058386b0585add2d75a3bcf30914f26a
George kuznecov <georgijkuznecov987@gmail.com>
2023-04-24 23:53:56 +0300

    Исправил

--- slice.js
+++ slice.js
@@ -8,7 +8,6 @@ const playSeasonsGame = () => {
         "Апельсин",
         "Мандарин",
     ];
-let arrList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 arr = arr.sort(() => Math.random() - 0.5);
 alert(arr)
 let first = prompt('Чему равнялся первый элемент массива?',);
@@ -17,15 +16,11 @@ alert(first)
 let last = prompt('Чему равнялся последний элемент массива?',);
 alert(last)
 
-function right() {
-    if ((first == arr[0]) & (last == arr[6])) {
-        alert("Вы угадали оба елмента");
-    }
-    else if ((first == arr[0]) & (last == arr[6])) {
-        alert("Вы угадали один элемент");
-    }
-    else ((first == arr[0]) & (last == arr[6])) {
-        alert("Вы не угадали ни одного элемента,");
-    }
+if (first === arr[0] && last === arr[arr.length - 1]) {
+  alert("Вы угадали оба елмента");
+} else if (first === arr[0] && last === arr[arr.length - 1]) {
+  alert("Вы угадали один элемент");
+} else {
+  alert("Вы не угадали ни одного элемента");
 }
 }
